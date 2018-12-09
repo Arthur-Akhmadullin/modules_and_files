7b89f99b7200b27736fa5c1f65138db58ff2b514
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2018-12-09 19:06:06 +0500

    add constructions "with ... as" and "try ... except ..."

--- 3/3.2.7.2.py
+++ 3/3.2.7.2.py
@@ -2,20 +2,26 @@ import random
 
 sum = 0
 x = random.randint(1,10)
-y = random.randint(1,10)
+y = random.randint(1,2)
 f1 = str(x) + ".txt"
 f2 = str(y) + ".txt"
 
 print("Открываем файлы: ", f1, ", ", f2)
 
-file_1 = open(f1, 'rt')
-for s in file_1:
-    sum += int(s)
+with open(f1, 'rt') as file_1:
+    try:
+        for s in file_1:
+            sum += int(s)
+    except Exception:
+        print("Ошибка содержимого в файле 1")
 file_1.close()
 
-file_2 = open(f2, 'rt')
-for s in file_2:
-    sum += int(s)
+with open(f2, 'rt') as file_2:
+    try:
+        for s in file_2:
+            sum += int(s)
+    except Exception:
+        print("Ошибка содержимого в файле 2")
 file_2.close()
 
 print("Сумма шести чисел = ", sum)
