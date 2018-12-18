f7c3a0205d921c32c0b57f8c2bf690da5439c974
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2018-12-18 23:14:11 +0500

    ???

--- 5/lesson_xml.py
+++ 5/lesson_xml.py
@@ -11,21 +11,22 @@ def tag_value(node, tagname, array):
             tag_value(node[i], tagname, array)
     return(array)
 
-count = 0
-def attrib_value(node, attribname):
-    global count
+
+def attrib_value(node, attribname, sum):
+
     for i in range(len(node)):
         if attribname in node[i].attrib.keys():
-            count += 1
-        if len(node[i]) > 0:
-            attrib_value(node[i], attribname)
-    return(count)
+            sum += 1
 
-#sdfsdfsdfsa
+        if len(node[i]) > 0:
+            attrib_value(node[i], attribname, sum)
+
+    return(sum)
+
 
 if __name__ == '__main__':
     text_list = []
     tag_value(root, "pc_item", text_list)
     print("Список значений:", text_list)
 
-    print("Количество узлов с заданным атрибутом =", attrib_value(root, "name"))
+    print("Количество узлов с заданным атрибутом =", attrib_value(root, "name", 0))
