2b9a6115679f08a7b1c7c5776d610c33a85708c1
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2019-01-04 17:28:42 +0500

    variable names changed

--- 7/7.4.1.py
+++ 7/7.4.1.py
@@ -2,10 +2,10 @@ import os.path
 
 def structure_catalog(arr_dir, arr_file):
     for root, dirs, files in os.walk('structure'):
-        for dir in dirs:
-            arr_dir.append(dir)
-        for file in files:
-            arr_file.append(file)
+        for d in dirs:
+            arr_dir.append(d)
+        for f in files:
+            arr_file.append(f)
     return(arr_dir, arr_file)
 
 array_dirs = []
@@ -13,9 +13,9 @@ array_files = []
 structure_catalog(array_dirs, array_files)
 
 print("СПИСОК КАТАЛОГОВ:")
-for dir in array_dirs:
-    print(dir)
+for d in array_dirs:
+    print(d)
 
 print("СПИСОК ФАЙЛОВ:")
-for file in array_files:
-    print(file)
+for f in array_files:
+    print(f)
--- 7/7.4.2.py
+++ 7/7.4.2.py
@@ -4,8 +4,8 @@ def delete_catalog(cat_path):
     if os.path.isdir(cat_path):
         for root, dirs, files in os.walk(cat_path):
             if dirs == []:
-                for file in files:
-                    os.remove(root+"/"+file)
+                for f in files:
+                    os.remove(root+"/"+f)
                 os.rmdir(root)
             else:
                 print("Каталог не пустой")
