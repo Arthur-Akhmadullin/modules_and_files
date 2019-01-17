e83341ab446888303558f6e204173e927a35aa26
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2019-01-18 00:06:20 +0500

    10-var2

---
+++ 10/10-var2.py
@@ -0,0 +1,16 @@
+from zipfile import ZipFile
+import os.path
+
+def add_archive(name, ext):
+    with ZipFile(name, "a") as archive:
+        for root, dirs, files in os.walk("."):
+            for filename in files:
+                if os.path.splitext(filename)[1] == ext:
+                    archive.write(filename)
+        archive.close()
+
+if __name__ == "__main__":
+    archive_name = "My.zip"
+    file_ext = ".txt"
+
+    add_archive(archive_name, file_ext)
