da461ca8960937f6c1e7ae63502d4810ac059495
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2019-01-20 22:26:56 +0500

    it is now possible to add files from subdirectories

--- 10/10-var2.py
+++ 10/10-var2.py
@@ -2,16 +2,19 @@ from zipfile import ZipFile
 import os.path
 
 def add_archive(name, ext):
-    with ZipFile(name, "a") as archive:
-        for root, dirs, files in os.walk("."):
-            for filename in files:
-                if os.path.splitext(filename)[1] == ext:
-                    print(os.path.basename(filename))
-                    archive.write(filename)
-        archive.close()
+    try:
+        with ZipFile(name, "a") as archive:
+            for root, dirs, files in os.walk("."):
+                for filename in files:
+                    if os.path.splitext(filename)[1] == ext:
+                        archive.write(os.path.join(root, filename), filename)
+            archive.close()
+    except Exception:
+        print("Ошибка добавления в архив")
+
 
 if __name__ == "__main__":
-    archive_name = "My.zip"
-    file_ext = ".txt"
 
+    archive_name = "My_archive.zip"
+    file_ext = ".txt"
     add_archive(archive_name, file_ext)
